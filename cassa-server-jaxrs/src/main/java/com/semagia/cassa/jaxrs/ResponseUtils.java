5e37d80f2eb71b0a731d1450255a67fa990babe8
Lars Heuer <heuer@semagia.com>
2011-05-18 18:06:33 +0200

    Removed unused import

--- cassa-server-jaxrs/src/main/java/com/semagia/cassa/jaxrs/ResponseUtils.java
+++ cassa-server-jaxrs/src/main/java/com/semagia/cassa/jaxrs/ResponseUtils.java
@@ -17,7 +17,6 @@ package com.semagia.cassa.jaxrs;
 
 import java.util.List;
 
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 

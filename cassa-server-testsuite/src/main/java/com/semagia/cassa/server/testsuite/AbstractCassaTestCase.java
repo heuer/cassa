140e871dcf8d0b9277731b978627bed01a49fec9
heuer <heuer@semagia.com>
2011-05-23 00:19:46 +0200

    Added some helper interfaces/classes, extended the abstract test case

--- cassa-server-testsuite/src/main/java/com/semagia/cassa/server/testsuite/AbstractCassaTestCase.java
+++ cassa-server-testsuite/src/main/java/com/semagia/cassa/server/testsuite/AbstractCassaTestCase.java
@@ -16,6 +16,11 @@
 package com.semagia.cassa.server.testsuite;
 
 import java.io.InputStream;
+import java.net.URI;
+
+import com.semagia.cassa.client.Graph;
+import com.semagia.cassa.client.GraphClient;
+import com.semagia.cassa.common.MediaType;
 
 import junit.framework.TestCase;
 
@@ -24,9 +29,64 @@ import junit.framework.TestCase;
  * 
  * @author Lars Heuer (heuer[at]semagia.com) <a href="http://www.semagia.com/">Semagia</a>
  */
-public abstract class AbstractCassaTestCase extends TestCase {
+public abstract class AbstractCassaTestCase extends TestCase implements IConstants {
+
+    protected static final MediaType MEDIATYPE_ANY = MediaType.valueOf("*/*");
+
+    protected GraphClient _client;
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        _client = new GraphClient(TestUtils.getServiceEndpoint());
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        _client = null;
+    }
 
     protected final InputStream getInputStream(final String testFile) {
         return this.getClass().getResourceAsStream(testFile);
     }
+
+    public void testDefaultGraphExists() throws Exception {
+        assertTrue(_client.existsGraph());
+    }
+
+    public void testDefaultGraphDeleteExists() throws Exception {
+        assertTrue(_client.existsGraph());
+        assertTrue(_client.deleteGraph() != null);
+        assertTrue(_client.existsGraph());
+    }
+
+    public void testGetDefaultGraph() throws Exception {
+        assertNotNull(_client.getGraph(MEDIATYPE_ANY));
+    }
+
+    public void assertGraphGET(final URI graphURI) throws Exception {
+        assertNotNull(_client.getGraph(graphURI, MEDIATYPE_ANY));
+    }
+
+    public void assertGraphExists(final URI graphURI) throws Exception {
+        assertTrue(_client.existsGraph(graphURI));
+    }
+
+    public void assertGraphGetMediaType(final URI graphURI, final MediaType mediaType) throws Exception {
+        final Graph graph = _client.getGraph(graphURI, mediaType);
+        assertNotNull(graph);
+        assertEquals(mediaType, graph.getMediaType());
+    }
+
+    public void assertGraphDelete(final URI graphURI) throws Exception {
+        assertTrue(_client.deleteGraph(graphURI) != null);
+    }
+
 }

89c7d6b5e21eb7e5ddbb0e8e7f87b6191aabdc66
heuer <heuer@semagia.com>
2011-06-04 12:39:11 +0200

    Added exception mappers

---
+++ cassa-server-jaxrs/src/main/java/com/semagia/cassa/jaxrs/providers/QueryExceptionMapper.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2011 Lars Heuer (heuer[at]semagia.com). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.semagia.cassa.jaxrs.providers;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.semagia.cassa.server.store.QueryException;
+
+/**
+ * Converts a {@link QueryException} into a HTTP Bad Request error.
+ * 
+ * @author Lars Heuer (heuer[at]semagia.com) <a href="http://www.semagia.com/">Semagia</a>
+ */
+@Provider
+public class QueryExceptionMapper implements ExceptionMapper<QueryException> {
+
+    /* (non-Javadoc)
+     * @see javax.ws.rs.ext.ExceptionMapper#toResponse(java.lang.Throwable)
+     */
+    @Override
+    public Response toResponse(QueryException ex) {
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+}

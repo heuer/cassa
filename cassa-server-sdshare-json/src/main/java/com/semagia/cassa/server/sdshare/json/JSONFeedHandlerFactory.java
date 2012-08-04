187c5198edeb29594f8da2cf5103b429b18356d1
Lars <heuer@semagia.com>
2012-08-04 23:51:36 +0200

    Renamed package

--- cassa-server-sdshare-json/src/main/java/com/semagia/cassa/server/sdshare/json/JSONFeedHandlerFactory.java
+++ cassa-server-sdshare-json/src/main/java/com/semagia/cassa/server/sdshare/json/JSONFeedHandlerFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.semagia.cassa.server.feed.json;
+package com.semagia.cassa.server.sdshare.json;
 
 import com.semagia.cassa.common.MediaType;
 import com.semagia.cassa.server.sdshare.IOutputAwareFeedHandler;

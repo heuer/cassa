01a7e2b566652617584c828c38326ad29c2e85c7
Lars <heuer@semagia.com>
2012-08-05 00:28:29 +0200

    Updated TMAPIStore to adapt changes of issue #4

--- cassa-store-tmapi/src/main/java/com/semagia/cassa/server/store/tmapi/TMAPIStore.java
+++ cassa-store-tmapi/src/main/java/com/semagia/cassa/server/store/tmapi/TMAPIStore.java
@@ -29,18 +29,18 @@ import org.tmapi.core.TopicMap;
 import org.tmapi.core.TopicMapSystem;
 
 import com.semagia.cassa.common.MediaType;
-import com.semagia.cassa.common.dm.IGraphInfo;
 import com.semagia.cassa.common.dm.IWritableRepresentation;
 import com.semagia.cassa.common.dm.RemovalStatus;
-import com.semagia.cassa.common.dm.impl.DefaultGraphInfo;
 import com.semagia.cassa.server.store.GraphMismatchException;
 import com.semagia.cassa.server.store.GraphNotExistsException;
+import com.semagia.cassa.server.store.IGraphInfo;
 import com.semagia.cassa.server.store.IStore;
 import com.semagia.cassa.server.store.ParseException;
 import com.semagia.cassa.server.store.QueryException;
 import com.semagia.cassa.server.store.StoreException;
 import com.semagia.cassa.server.store.UnsupportedMediaTypeException;
 import com.semagia.cassa.server.store.impl.AbstractStore;
+import com.semagia.cassa.server.store.impl.DefaultGraphInfo;
 
 /**
  * {@link IStore} implementation that uses a TMAPI-compatible Topic Maps engine.

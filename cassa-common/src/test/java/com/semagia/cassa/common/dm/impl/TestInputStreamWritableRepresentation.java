3fc10034824f7036f1af1fbf8035cb129afd9beb
Lars <heuer@semagia.com>
2012-08-08 22:08:11 +0200

    Removed unused import

--- cassa-common/src/test/java/com/semagia/cassa/common/dm/impl/TestInputStreamWritableRepresentation.java
+++ cassa-common/src/test/java/com/semagia/cassa/common/dm/impl/TestInputStreamWritableRepresentation.java
@@ -19,7 +19,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Arrays;
 
 import com.semagia.cassa.common.MediaType;
